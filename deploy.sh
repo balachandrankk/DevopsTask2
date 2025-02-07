d785e85d8273ea7e6fb81a97efa9f9ded166e139
balachandrankk <127740036+balachandrankk@users.noreply.github.com>
2025-02-07 13:32:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u balachandran2005 -p 19-May-05
-    docker tag test balachandran2005/ar
-    docker push balachandran2005/ar
+    docker tag test balachandran2005/task2
+    docker push balachandran2005/task2
     
